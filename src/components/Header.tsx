206ca8d00ca2755748eb2337d19828e7d6854288
Ryan <saltburn6969@gmail.com>
2025-11-24 23:55:48 +0530

    comit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,7 +61,7 @@ export const Header = () => {
         data-border="rounded"
       >
         <Flex paddingLeft="12" fillWidth vertical="center" textVariant="body-default-s">
-          {display.location && <Flex hide="s">{person.location}</Flex>}
+          {display.location && <Flex hide="s">{}</Flex>}
         </Flex>
         <Flex fillWidth horizontal="center">
           <Flex
@@ -129,7 +129,7 @@ export const Header = () => {
             textVariant="body-default-s"
             gap="20"
           >
-            <Flex hide="s">{display.time && <TimeDisplay timeZone={person.location} />}</Flex>
+            
           </Flex>
         </Flex>
       </Flex>
